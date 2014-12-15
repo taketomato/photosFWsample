81a48af25a5426b48a17684700e2907c96c2fc72
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-12-15 10:08:22 +0900

    Initial Commit

---
+++ photosFWsample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  photosFWsample
+//
+//  Created by tchiba on 2014/12/15.
+//  Copyright (c) 2014年 takeotmato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
